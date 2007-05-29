3a88b38d749d6b47beafed6fcdba26b2a9ee3495
David Precious <davidp@preshweb.co.uk>
2007-05-29 20:23:08 +0000

    Version 0.02 of Lyrics::Fetcher::LyricWiki - documentation fixes and removing redundant parse() sub.  In my haste to get the first version out, I left some references to AZLyrics.com in there (I copied + modified that module) along with the whole parse() sub.  Arse.
    git-svn-id: file:///shared/svn/scripts/perl/modules/Lyrics-Fetcher-LyricWiki@143 c1e6fe2f-0919-0410-96f0-e3221d39872c

--- lib/Lyrics/Fetcher/LyricWiki.pm
+++ lib/Lyrics/Fetcher/LyricWiki.pm
@@ -8,7 +8,7 @@ use warnings;
 use SOAP::Lite;
 use Carp;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 # the HTTP User-Agent we'll send:
 our $AGENT = "Perl/Lyrics::Fetcher::LyricWiki $VERSION";
@@ -52,76 +52,6 @@ sub fetch {
 }
 
 
-sub parse {
-    
-    my $html = shift;
-    my $tp = HTML::TokeParser->new(\$html);
-    #my $text = $tp->get_trimmed_text();
-    
-    
-    # the HTML on the pages returned by azlyrics.com is rather nasty, so we
-    # have to do dirty tricks to parse it.  We'll find all <font> tags, and read
-    # from there until the </font> tag.  If what we got looks vaguely suitable,
-    # we then need to trim it a little.
-    while (my $wotsit = $tp->get_tag('font')) {
-        
-        # get the text up to the next </font> tag
-        my $text = $tp->get_text('/font');
-        
-        if (length $text > 20) {
-            # this might well be it.  We'll clean it up and do some checks on
-            # it in the process.
-            
-            $text =~ s/\r//mg;
-            
-            # the page title should look like "<ARTIST> LYRICS" on a line
-            # by itself:
-            unless ($text =~ s/^.*LYRICS \n?//xgs) {
-                carp("No page title found, this HTML doesn't look right");
-                return;
-            }
-           
-            
-            unless ($text =~ s/\[ \s www\.azlyrics\.com \s \]//xmg) {
-                carp("No azlyrics.com line found");
-                return;
-            }
-            
-            # song title should be on a line that starts and ends with double
-            # quotes, strip it out
-            unless ($text =~ s/" .+ "//xmg) {
-                carp("No song title found, this HTML doesn't look right");
-                return;
-            }
-            
-            # some lyrics pages have credits at the bottom for the submitter...
-            # remove them from the lyrics, but store them in case they're
-            # wanted:
-            my @credits;
-            while ($text =~ s{\[ Thanks \s to \s (.+) \]}{}xgi) {
-                push @credits, $1;
-            }
-            
-            # bodge... do this twice, to avoid the '... used only once' warning
-            @Lyrics::Fetcher::azcredits = @credits;
-            @Lyrics::Fetcher::azcredits = @credits;
-            
-            # finally, clear up excess blank lines:
-            while ($text =~ s/\n{2,}/\n/gs) {};
-
-            
-            
-            return $text;
-            
-        }
-        
-    
-    
-    }
-    
-} # end of sub parse
-
-
 
 1;
 __END__
@@ -143,13 +73,13 @@ Lyrics::Fetcher::LyricWiki - Get song lyrics from www.LyricWiki.org
 
 =head1 DESCRIPTION
 
-This module tries to get song lyrics from www.azlyrics.com.  It's designed to
+This module tries to get song lyrics from www.lyricwiki.org.  It's designed to
 be called by Lyrics::Fetcher, but can be used directly if you'd prefer.
 
 
 =head1 BUGS
 
-Probably.  Coded in about an hour whilst drinking a cold can of Fosters :)
+Probably.  Coded in about an hour whilst drinking cold lager :)
 If you find any bugs, please let me know.
 
 
